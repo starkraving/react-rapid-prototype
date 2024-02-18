dea0022bf3bb725654de896aef95cf07b3148013
Mike Ritchie <mike.ritchie@ticketops.com>
2024-02-17 17:47:53 -0800

    Added env vars for project, link locations and template, preparing for node server

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import AppTemplate from './apptemplate/template';
-import project from './strata.json';
+import React, { Suspense, useEffect, useState } from 'react';
 import {ReactRapidPrototype} from 'react-rapid-prototype';
+
+const LazyLoadedApp = () => {
+  const [project, setProject] = useState(null);
+  const [AppTemplate, setTemplate] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const importProject = import(process.env.REACT_APP_PATH_TO_PROJECT);
+    const importAppTemplate = import(process.env.REACT_APP_PATH_TO_TEMPLATE);
+    let _project;
+    let _AppTemplate;
+    Promise.all([importProject, importAppTemplate]).then((imported) => {
+      [{default: _project}, {default: _AppTemplate}] = imported;
+      setProject(_project);
+      setTemplate(() => _AppTemplate);
+      return setLoaded(true);
+    });
+  }, []);
+
+  return (
+    loaded && <ReactRapidPrototype
+      project={project}
+      renderProject={AppTemplate}
+      linkLocations={JSON.parse(process.env.REACT_APP_LINK_LOCATIONS)}
+    />
+  );
+};
 
 function App() {
   return (
-    <ReactRapidPrototype
-     project={project}
-     renderProject={AppTemplate}
-     linkLocations={['topNav', 'footer', 'general', 'homecal', 'card']}
-     />
-  );
+    <Suspense fallback={<div>Loading...</div>}>
+      <LazyLoadedApp />
+    </Suspense>
+  )
 }
 
 export default App;
